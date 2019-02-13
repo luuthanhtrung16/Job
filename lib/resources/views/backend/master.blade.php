65186c221b5b7150d1c5fa6c553ea4b513dcf958
LuuTrungg <luuthanhtrung16@gmail.com>
2019-02-13 14:38:51 +0700

    13/2 thay doi master

--- lib/resources/views/backend/master.blade.php
+++ lib/resources/views/backend/master.blade.php
@@ -194,18 +194,13 @@
 			<div class="menu-sidebar__content js-scrollbar1">
 				<nav class="navbar-sidebar">
 					<ul class="list-unstyled navbar__list">
-						<li class="has-sub">
-							<a class="js-arrow" href="#">
-								<i class="fas fa-tachometer-alt"></i>Dashboard
-							</a>
-							
-						</li>
+						
 						<li>
 							<a href="chart.html">
 								<i class="fas fa-chart-bar"></i>Quản lý người dùng
 							</a>
 						</li>
-						<li>
+						<!-- <li>
 							<a href="table.html">
 								<i class="fas fa-table"></i>Quản lý job
 							</a>
@@ -304,7 +299,7 @@
 									</a>
 								</li>
 							</ul>
-						</li>
+						</li> -->
 					</ul>
 				</nav>
 			</div>
